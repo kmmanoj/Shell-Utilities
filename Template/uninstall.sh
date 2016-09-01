6dc8278a4949974557bd7e8a598cffca11269785
K M Manoj vignesh <kmmanoj1990@gmail.com>
2016-09-02 00:18:00 +0530

    Delete uninstall.sh

--- Template/uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /usr/bin
-sudo rm -i *exe 2> /dev/null
-sudo rm exe 2> /dev/null
-
-echo "Finishing ...";
-echo "\n\033[0;32m\033[1m============UnInstalled autocompiler utility==============\033[0m";
