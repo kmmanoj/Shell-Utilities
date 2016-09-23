2de41e30685cf1517d911efbdcaf475740b3bced
K M Manoj vignesh <kmmanoj1990@gmail.com>
2016-09-23 18:55:55 +0530

    Update uninstall.sh

--- Template/uninstall.sh
+++ Template/uninstall.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 echo -e "\n\033[0;35m\033[1m============Template utility==============\033[0m";
-cd /usr/bin
-sudo rm -f program
-echo "Removed Command program";
-cd ~/Templates
-sudo rm javaheader 2> /dev/null
-sudo rm java 2> /dev/null
-sudo rm html 2> /dev/null
-sudo rm c 2> /dev/null
-sudo rm cpp 2> /dev/null
-sudo rm asm 2> /dev/null
-echo "Removed"
+sudo rm -f /usr/bin/program
+echo "Removed Command 'program'";
+sudo rm ~/Templates/javaheader 2> /dev/null
+sudo rm ~/Templates/java 2> /dev/null
+sudo rm ~/Templates/html 2> /dev/null
+sudo rm ~/Templates/c 2> /dev/null
+sudo rm ~/Templates/cpp 2> /dev/null
+sudo rm ~/Templates/asm 2> /dev/null
+echo "Removed Templates!"
 echo -e "\n\033[0;31m\033[1m============Uninstalled Template utility==============\033[0m";
