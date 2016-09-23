cc02c14c7800c88ac4255581cca580ef71b6abcb
K M Manoj vignesh <kmmanoj1990@gmail.com>
2016-09-23 18:54:19 +0530

    Update install.sh

--- Template/install.sh
+++ Template/install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 echo -e "\n\033[0;35m\033[1m============Template utility==============\033[0m";
 sudo cp ./program /usr/bin/
-ls ~/Templates/ &> /dev/null
-status=$?
-if [ $status -ne 0 ];then 
-	mkdir ~/Templates
+if [ ! -d ~/Templates ];
+then 
+	mkdir ~/Templates;
+	echo "Creating Templates folder";
+else
+	echo "Templates folder already exists";
 fi
-ls ~/Templates
-sudo cp ./Templates/* ~/Templates/*
+sudo cp ./Templates/* ~/Templates/
+echo "Templates Filled!!!";
 echo -e "\n Feel free edit the Templates in ~/Templates/ directory as per your usage";
 echo -e "\n\033[0;32m\033[1m============Installed Templates utility==============\033[0m";
 
